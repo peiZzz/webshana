057caae17330c25d847235ca1d6ea628b059b448
peiZzz <578414658@qq.com>
2018-01-11 13:05:13 +0800

    add regitser

--- config/config.js
+++ config/config.js
@@ -2,3 +2,15 @@ angular.module('app')
 	.config(['$ionicConfigProvider', function ($ionicConfigProvider) {
 		 $ionicConfigProvider.platform.android.tabs.position('bottom');
 	}])
+	.config(["$httpProvider", function ($httpProvider) {
+　　//更改 Content-Type
+    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
+    $httpProvider.defaults.headers.post["Accept"] = "*/*";
+    $httpProvider.defaults.transformRequest = function (data) { 
+    //把JSON数据转换成字符串形式
+      if (data !== undefined) { 
+        return $.param(data);
+      }
+        return data;
+      };
+  }])
--- views/rigister/rigisterController.js
+++ views/rigister/rigisterController.js
@@ -1,15 +1,32 @@
 angular.module('app')
-	.controller('rigisterController',['$scope','validForm','tip','api',function($scope,validForm,tip,api){
+	.controller('rigisterController',['$scope','validForm','tip','api','formatTime','$interval',function($scope,validForm,tip,api,formatTime,$interval){
 		$scope.userInfo = {
 			phone:'',
 			pwd:'',
 			rePwd:'',
-			code:'1234'
+			code:'',
+			registerTime:'',
+			agree:100
 		};	
 
-		$scope.register = function(){
+		$scope.code = '';
+
+		$scope.isPass = false;
+        //注册验证 
+		$scope.register = function(){						
 			if(!isValid()){return}//不通过就拦截掉
-			
+
+			$scope.userInfo.registerTime = formatTime.format(new Date(),'yyyy-MM-dd hh:mm:ss');	
+			tip.loadTips.showLoading();
+			api.fetchPost('http://127.0.0.1:9000/register',$scope.userInfo)
+				.then(function(data){
+					console.log(data);
+					tip.loadTips.hideLoading();
+				})
+				.catch(function(err){
+					console.log('出错了');
+					tip.loadTips.hideLoading();
+				})
 		}
 
 		function isValid(){
@@ -31,19 +48,50 @@ angular.module('app')
 			}else if (!validForm.isEqual($scope.userInfo.pwd,$scope.userInfo.rePwd)){
 				tip.showTip({msg:'两次密码不一致'});
 				return false
+			}else if (!validForm.isNotEmpty($scope.userInfo.code)){
+				tip.showTip({msg:'验证码不能为空'});
+				return false
+			}else if (!validForm.isEqual($scope.userInfo.code,$scope.code)){
+				tip.showTip({msg:'验证码不正确'});
+				return false
 			}
 
 			return true;
 		}
-
+        // 发送短信
+        $scope.description = "获取验证码";
+		var second = 59;
+		var timerHandler;
 		$scope.getValidCode = function () {
+			if(timerHandler){return}
+			timerHandler = $interval(function(){
+				$scope.show = true;
+				if(second <= 0){
+					$interval.cancel(timerHandler);
+					second = 59;
+					$scope.description = "获取验证码";
+					$scope.show = false;
+				}else{
+					$scope.description = second + "秒后重发";
+					second--;
+				}
+			},1000,100)
+			tip.loadTips.showLoading();
 			api.fetchGet('http://127.0.0.1:9000/message', {phone: $scope.userInfo.phone})
 				.then(function (data) {
-					console.log(data);
+					$scope.code = data.data.code;
+					console.log($scope.code)
+					tip.loadTips.hideLoading();
 				})
-				.catch(function (err) {
-					console.log(err);
+				.catch(function(err){
+					console.log('cll');
+					tip.loadTips.hideLoading();
 				})
+				
+		}
+
+		$scope.isPhone = function(){
+			$scope.isPass = validForm.validPhone($scope.userInfo.phone);
 		}
 		
 	}])
